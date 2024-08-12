0ae8f34fdf7fd97190d4a141b31210bdb1beb66c
k1sihyeon <tlgus530@naver.com>
2024-08-13 00:34:30 +0900

    #9 product class 구현

--- product/product.h
+++ product/product.h
@@ -1,21 +1,32 @@
 #pragma once
 
-#include <iostream>
-
 using namespace std;
 
 class Product {
    private:
+    static int _sharedId;   //singleton? 같은 클래스면 모두 접근 가능
+    bool isInit = false;
+
+   protected:
     int _id;
     string _name;
+    int _price;
     string _category;
     string _company;
     int _totalPurchase;
 
    public:
+    // generator
+    Product();
+    Product(string name, int price, string category, string company);
+
+    // destructor
+    ~Product();
+
     // getter
     int getId();
     string getName();
+    int getPrice();
     string getCategory();
     string getCompany();
     int getTotalPurchase();
@@ -23,12 +34,14 @@ class Product {
     // setter
     void setId(int id);
     void setName(string name);
+    void setPrice(int price);
     void setCategory(string category);
     void setCompany(string company);
     void setTotalPurchase(int count);
-    void increasePurchase();        
+    void increasePurchase();
     //_totalPurchase 변수 1 증가
 
     // id 생성
     int generateId();
+    void initSharedId();
 };
